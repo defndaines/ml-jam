4569fe1e151000edca9c52ff115f69670fde6f16
defndaines <defndaines@gmail.com>
2013-07-12 09:30:15 -0500

    Reorganized to keep implementations together for now.

--- src/ml_jam/core.clj
+++ src/ml_jam/core.clj
@@ -14,35 +14,16 @@
   [line]
   (map read-string (clojure.string/split line #",")))
 
-(defn line->array
-  "Convert a comma-separate string into a sequence."
-  [line]
-  (int-array (map read-string (clojure.string/split line #","))))
-
-(defn hip-data
-  [line]
-  (let [[label & values] (map read-string (clojure.string/split line #","))]
-    (list label (int-array values))))
-
 (defn read-data
   "Pull the data from the file into a format for processing."
   [path]
   (map line->seq (get-lines path)))
 
-(defn read-data-into-array
-  "Pull the data from the file into a format for processing."
-  [path]
-  (map hip-data (get-lines path)))
-
 ; 5000 known values.
 (def training-set (read-data "digitssample.csv"))
 
-(def hip-train (read-data-into-array "digitssample.csv"))
-
 ; 500 examples to test against.
 (def validation-set (read-data "digitscheck.csv"))
-
-(def hip-validation (read-data-into-array "digitscheck.csv"))
 
 (defn sqr-diff
   "Find the difference between numbers and square it."
@@ -54,11 +35,6 @@
   [left right]
   (reduce + (map sqr-diff left right)))
 
-(defn hip-distance
-  [left right]
-  (int/asum
-    (int/amap [l left r right] (* (- l r) (- l r)))))
-
 (defn nearest-neighbor
   [candidate known]
   (let [[expect & image] candidate]
@@ -66,14 +42,6 @@
       (sort-by second
                (for [[label & values] known]
                  [label (euclidean-distance image values)])))))
-
-(defn hippest-neighbor
-  [candidate known]
-  (let [[expect & image] candidate]
-    (first
-      (sort-by second
-               (for [[label & values] known]
-                 [label (hip-distance image values)])))))
 
 (defn nearest-neighbor-map
   [candidate known]
@@ -83,3 +51,37 @@
                (map
                  #(list (first %) (euclidean-distance image (rest %)))
                  known)))))
+
+;; hiphip Versions
+
+(defn line->array
+  "Convert a comma-separate string into a sequence."
+  [line]
+  (int-array (map read-string (clojure.string/split line #","))))
+
+(defn hip-data
+  [line]
+  (let [[label & values] (map read-string (clojure.string/split line #","))]
+    (list label (int-array values))))
+
+(defn read-data-into-array
+  "Pull the data from the file into a format for processing."
+  [path]
+  (map hip-data (get-lines path)))
+
+(def hip-train (read-data-into-array "digitssample.csv"))
+
+(def hip-validation (read-data-into-array "digitscheck.csv"))
+
+(defn hip-distance
+  [left right]
+  (int/asum
+    (int/amap [l left r right] (* (- l r) (- l r)))))
+
+(defn hippest-neighbor
+  [candidate known]
+  (let [[expect & image] candidate]
+    (first
+      (sort-by second
+               (for [[label & values] known]
+                 [label (hip-distance image values)])))))
